6c6eee351383aabcdfde73d3e353408952770b7e
Wiktor Religo <wiciorny@gmail.com>
2021-03-07 18:24:12 +0100

    Initial commit, set up project, basic dependencies

---
+++ api-core/src/main/java/com/jadevirek/prescriptionapp/PrescriptionManagementApplication.java
@@ -0,0 +1,13 @@
+package src.main.java.com.jadevirek.prescriptionapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrescriptionManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrescriptionManagementApplication.class, args);
+	}
+
+}
---
+++ api-core/src/test/java/com/jadevirek/prescriptionapp/PrescriptionManagementApplicationTests.java
@@ -0,0 +1,15 @@
+package src.test.java.com.jadevirek.prescriptionapp;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class PrescriptionManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
