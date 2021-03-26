9a77844b9df2755c4a371e4e92a97430a3380ffe
Wiktor Religo <wiciorny@gmail.com>
2021-03-26 22:42:35 +0100

    adding handlers package- on work

--- api-core/src/main/java/com/jadevirek/prescriptionapp/handlers/ExceptionErrorInfo.java
+++ api-core/src/main/java/com/jadevirek/prescriptionapp/handlers/ExceptionErrorInfo.java
@@ -1,4 +1,30 @@
 package src.main.java.com.jadevirek.prescriptionapp.handlers;
 
-public class ExceptionErrorInfo {
+import src.main.java.com.jadevirek.prescriptionapp.common.CreateEntityException;
+
+/**
+ * ExceptionErrorInfo immutable class is used to holding output information about an Error
+ */
+public final class ExceptionErrorInfo {
+
+    private static ExceptionErrorInfo thisInstance;
+    private final String error;
+    public ExceptionErrorInfo(String error) {
+        this.error = error;
+    }
+
+    public static ExceptionErrorInfo getInstance(String cause) {
+        if (thisInstance == null) {
+            synchronized (CreateEntityException.class) {
+                if (thisInstance == null) {
+                    thisInstance = new ExceptionErrorInfo(cause);
+                }
+            }
+        }
+        return thisInstance;
+    }
+
+    public String getError() {
+        return error;
+    }
 }
--- api-core/src/main/java/com/jadevirek/prescriptionapp/handlers/GlobalExceptionHandler.java
+++ api-core/src/main/java/com/jadevirek/prescriptionapp/handlers/GlobalExceptionHandler.java
@@ -1,4 +1,51 @@
 package src.main.java.com.jadevirek.prescriptionapp.handlers;
 
-public class GlobalExceptionHandler {
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import src.main.java.com.jadevirek.prescriptionapp.common.CreateEntityException;
+import src.main.java.com.jadevirek.prescriptionapp.common.EntityNotFoundException;
+
+/**
+ * GlobalExceptionHandler class
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Object> handleIllegalArgumentException(IllegalArgumentException ex,
+            WebRequest request) {
+
+        return handleExceptionInternal(ex, new EntityNotFoundException(ex.getMessage()),
+                new HttpHeaders(),
+                HttpStatus.NOT_FOUND, request);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Object> handleNoEntityFoundException(EntityNotFoundException ex,
+            WebRequest request) {
+        return handleExceptionInternal(ex, new EntityNotFoundException(ex.getMessage()),
+                new HttpHeaders(),
+                HttpStatus.BAD_REQUEST, request);
+    }
+
+    @ExceptionHandler(CreateEntityException.class)
+    public ResponseEntity<Object> handleCreateEntityException(CreateEntityException ex,
+            WebRequest request) {
+        return handleExceptionInternal(ex, new ExceptionErrorInfo(ex.getMessage()),
+                new HttpHeaders(),
+                HttpStatus.BAD_REQUEST, request);
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<Object> handleIllegalStateException(IllegalStateException ex,
+            WebRequest request) {
+        return handleExceptionInternal(ex, new ExceptionErrorInfo(ex.getMessage()),
+                new HttpHeaders(),
+                HttpStatus.NOT_FOUND, request);
+    }
 }
